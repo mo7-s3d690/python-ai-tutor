07d28222c1dff4b8ec6e974a22ee2497dcf88a52
Mohammed saad <deadline9904@gmail.com>
2025-11-28 13:20:30 +0200

    Add tasks 1 and 2 to the Python tutorial

--- my python toturial.py
+++ my python toturial.py
@@ -1,3 +1,4 @@
+#task 1 
 p1 =int(input("enter the first num"))
 p2 = int(input("enter the second num"))
 t_b = p1 + p2
@@ -7,4 +8,25 @@ if cost > t_b:
     print(f"we still need {a} pounds")
 else:
     a =t_b - cost
+
     print(f"we still have {a} pounds")
+'''----------------------------------------------------------'''
+# task 2 part 1
+x = int (  input ( "enter ur temperature in celsius: " ) )
+
+if x >= 30:
+    print ( "its a hot day , stay hydrated" )
+elif x >= 20 and x < 30:
+    print ( "its a nice day , enjoy the weather" )
+elif x >= 10 and x < 20:
+    print ( "its a bit cold , wear a jacket" )
+else:
+    print ( "its cold , dont go outside" )
+
+# task 2 part 2
+x = list(range(1 , 21))
+
+for i in x :
+    if i % 3 == 0:
+        print(f"{i} is divisible by 3")
+'''----------------------------------------------------------'''
